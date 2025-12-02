b6630af32809fb64a4594e7bcd297501436122c6
Saifkhan <Saifkhan26125@gmail.com>
2025-12-02 12:16:58 +0500

    Delete my portfolio directory

--- my portfolio/script.js
+++
@@ -1,8 +0,0 @@
-// Mobile menu toggle
-const menuBtn = document.querySelector('.menu-btn');
-const navLinks = document.querySelector('.nav-links');
-
-menuBtn.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
-
